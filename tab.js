7f5ae90c7ed8b9b78cc479887d8459ff71b1b5e2
jsk3342 <kss2460@naver.com>
2022-02-12 13:54:31 +0900

    연습

--- tab.js
+++ tab.js
@@ -1,50 +1,30 @@
-// $(".tab-button")
-//   .eq(0)
-//   .click(function () {
-//     $(".tab-button").removeClass("active");
-//     $(".tab-content").removeClass("show");
-//     $(".tab-button1").eq(0).addClass("active");
-//     $(".tab-content1").eq(0).addClass("show");
-//   });
-// $(".tab-button")
-//   .eq(1)
-//   .click(function () {
-//     $(".tab-button").removeClass("active");
-//     $(".tab-content").removeClass("show");
-//     $(".tab-button").eq(1).addClass("active");
-//     $(".tab-content").eq(1).addClass("show");
-//   });
-// $(".tab-button")
-//   .eq(2)
-//   .click(function () {
-//     $(".tab-button").removeClass("active");
-//     $(".tab-content").removeClass("show");
-//     $(".tab-button").eq(2).addClass("active");
-//     $(".tab-content").eq(2).addClass("show");
-//   });
-
 $(window).on("scroll", function () {
   if ($(window).scrollTop() > 100) {
     $(".nav-menu").addClass("nav-black");
   } else $(".nav-menu").removeClass("nav-black");
 });
 
-// 여기서 하나 클릭해도 계속 3번까지 돌아가는거 아닌가?
-// 작동원리 모르겠음
+// for (let i = 0; i < $(".tab-button").length; i++) {
+//   $(".tab-button")
+//     .eq(i)
+//     .click(function () {
+//       탭열기(i);
+//     });
+// }
 
-for (let i = 0; i < $(".tab-button").length; i++) {
-  $(".tab-button")
-    .eq(i)
-    .click(function () {
-      $(".tab-button").removeClass("active");
-      $(".tab-content").removeClass("show");
-      $(".tab-button").eq(i).addClass("active");
-      $(".tab-content").eq(i).addClass("show");
-    });
+function 탭열기(숫자) {
+  $(".tab-button").removeClass("active");
+  $(".tab-content").removeClass("show");
+  $(".tab-button").eq(숫자).addClass("active");
+  $(".tab-content").eq(숫자).addClass("show");
 }
 
 $(".black-background").click(function (e) {
   if (e.target == e.currentTarget) {
     $(".black-background").removeClass("trans");
   }
+});
+
+$(".list").click(function (e) {
+  탭열기(e.target.dataset.id);
 });
