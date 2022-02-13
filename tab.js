44abab6086bd9249179ed28502d828b004723783
jsk3342 <kss2460@naver.com>
2022-02-13 20:51:42 +0900

    배열 연습

--- tab.js
+++ tab.js
@@ -28,3 +28,13 @@ $(".black-background").click(function (e) {
 $(".list").click(function (e) {
   탭열기(e.target.dataset.id);
 });
+
+var 어레이 = ["BMW", 520];
+var 오브젝트 = { brand: "BMW", model: 520 };
+var 자료 = [{ brand: "BMW" }, { model: 520 }];
+$(".modelname").html(자료[0].brand);
+$(".modelmain").html(자료[1].model);
+
+$("#option1").on("change", function () {
+  if ()
+});
