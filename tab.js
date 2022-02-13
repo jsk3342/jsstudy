6974f487a7d4949808bfd46a2c0c087cca2e9b46
jsk3342 <kss2460@naver.com>
2022-02-14 03:16:45 +0900

    폼만들기

--- tab.js
+++ tab.js
@@ -35,6 +35,20 @@ var 자료 = [{ brand: "BMW" }, { model: 520 }];
 $(".modelname").html(자료[0].brand);
 $(".modelmain").html(자료[1].model);
 
+var 사이즈 = [26, 28, 30, 32, 34, 36, 38];
+var 셔츠사이즈 = [90, 95, 100, 105, 110, 115];
 $("#option1").on("change", function () {
-  if ()
+  if ($("select").val() == "셔츠") {
+    $("#option2").html("");
+    for (var i = 0; i < 셔츠사이즈.length; i++) {
+      var 템플릿 = `<option>${셔츠사이즈[i]}</option>`;
+      $("#option2").append(템플릿);
+    }
+  } else if ($("select").val() == "바지") {
+    $("#option2").html("");
+    사이즈.forEach(function (i) {
+      var 템플릿 = `<option>${[i]}</option>`;
+      $("#option2").append(템플릿);
+    });
+  }
 });
